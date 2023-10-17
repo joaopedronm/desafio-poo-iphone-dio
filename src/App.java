e393a9f0ef8ee74d4f8607e862a97329675ee487
João Pedro <joaopedron253@gmail.com>
2023-10-17 09:51:14 -0300

    desafio de projeto poo - primeiros rabiscos

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int num = 42;
+        int num2 = num + 10;
+
+        System.out.println(num2);
+    }
+}
